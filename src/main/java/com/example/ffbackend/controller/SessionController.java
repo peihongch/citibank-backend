74b6e58004f9dae7be8eb1d5177ada2d22647442
HeBo <hebomou@qq.com>
2019-08-31 16:30:52 +0800

    完成了session接口

--- src/main/java/com/example/ffbackend/controller/SessionController.java
+++ src/main/java/com/example/ffbackend/controller/SessionController.java
@@ -2,7 +2,6 @@ package com.example.ffbackend.controller;
 
 import com.example.ffbackend.viewobject.ResponseBean;
 
-import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/session")
 public class SessionController {
     @PostMapping
-    public ResponseBean<Pair<Long, String>> Login(@RequestParam(value = "username") String username,
+    public ResponseBean<String> CreateSession(@RequestParam(value = "username") String username,
             @RequestParam(value = "pwd") String pwd) {
-        return new ResponseBean<Pair<Long, String>>(true, Pair.of(23333L, "sample_token"));
+        return new ResponseBean<String>(true, "sample_token");
     }
 
-    @DeleteMapping("/{token}")
-    public ResponseBean<Object> Logout(@PathVariable("token") String token) {
+    @DeleteMapping("/{session}")
+    public ResponseBean<Object> DeleteSession(@PathVariable("session") String session) {
         return new ResponseBean<>(true, null);
     }
 }
