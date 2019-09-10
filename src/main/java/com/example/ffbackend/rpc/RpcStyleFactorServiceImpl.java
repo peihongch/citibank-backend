5e9da6420b6ece2150d59bbbc0a34f2071369c23
steve <chph13420146901@gmail.com>
2019-09-10 21:57:43 +0800

    bugfix: implement rpc service interfaces

--- src/main/java/com/example/ffbackend/rpc/RpcStyleFactorServiceImpl.java
+++ src/main/java/com/example/ffbackend/rpc/RpcStyleFactorServiceImpl.java
@@ -3,4 +3,23 @@ package com.example.ffbackend.rpc;
 import com.example.ffbackend.bl.RpcStyleFactorService;
 
 public class RpcStyleFactorServiceImpl implements RpcStyleFactorService {
+    @Override
+    public float getSize(String tsCode, String fields) {
+        return 0;
+    }
+
+    @Override
+    public float getMomentum(String tsCode) {
+        return 0;
+    }
+
+    @Override
+    public float getBookToMarket(String stockCode) {
+        return 0;
+    }
+
+    @Override
+    public float getLiquidity(String stockCode) {
+        return 0;
+    }
 }
