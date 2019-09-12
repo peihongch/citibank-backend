b3e86edfff01dd0672114865fe638a3697372073
HeBo <hebomou@qq.com>
2019-09-13 01:09:01 +0800

    试图添加api集成测试

---
+++ src/test/java/com/example/ffbackend/UserTests.java
@@ -0,0 +1,27 @@
+package com.example.ffbackend;
+
+import com.example.ffbackend.controller.UserController;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.var;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserTests {
+	@Autowired
+	UserController userController;
+
+	@Test
+	public void insertUserTest() {
+		// assertEquals(new long[] { 1L, 2L }, new long[] { 1L, 2L });
+		// assertArrayEquals(new long[] { 1L, 2L }, new long[] { 1L, 2L });
+		// var arr = new long[] { 1L, 2L };
+		// assertEquals(2, arr.length);
+	}
+
+}
