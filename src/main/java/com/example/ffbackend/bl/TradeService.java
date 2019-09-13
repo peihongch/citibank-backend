812f061faab8b0d075f200dc7773905ff4cb2a60
HeBo <hebomou@qq.com>
2019-09-13 15:40:13 +0800

    修复错误

--- src/main/java/com/example/ffbackend/bl/TradeService.java
+++ src/main/java/com/example/ffbackend/bl/TradeService.java
@@ -1,5 +1,6 @@
 package com.example.ffbackend.bl;
 
+import com.example.ffbackend.da.TransactionDaService;
 import com.example.ffbackend.vo.TradeVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradeService {
     @Autowired
-    
+    TransactionDaService da;
 
     public boolean CreateTrade (TradeVo vo) {
         return true;
