a349b4d2ea7088770e9a71c0befdc284f1fb7060
HeBo <hebomou@qq.com>
2019-09-14 01:14:58 +0800

    修复一个警告

--- src/main/java/com/example/ffbackend/stubs/RegularAdjustmentIndex.java
+++ src/main/java/com/example/ffbackend/stubs/RegularAdjustmentIndex.java
@@ -5,7 +5,9 @@ import com.example.ffbackend.vo.RegularAdjustmentIndexVo;
 import java.util.ArrayList;
 
 public class RegularAdjustmentIndex {
-    public static ArrayList<RegularAdjustmentIndexVo> UserIndex = new ArrayList<RegularAdjustmentIndexVo>(){{
+    public static ArrayList<RegularAdjustmentIndexVo> UserIndex = new ArrayList<RegularAdjustmentIndexVo>(){
+        private static final long serialVersionUID = -8701402112229466070L;
+        {
         add(new RegularAdjustmentIndexVo("1",0.0,10.0,0.0));
         add(new RegularAdjustmentIndexVo("2",1.0,10.0,0.0));
         add(new RegularAdjustmentIndexVo("3",5.0,100.0,0.0));
