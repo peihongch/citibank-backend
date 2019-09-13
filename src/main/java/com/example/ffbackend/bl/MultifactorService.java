b1d0d11d8d3d84621c894eb81c9d40c68afbb6ea
HeBo <hebomou@qq.com>
2019-09-13 19:43:05 +0800

    完成了随机化

--- src/main/java/com/example/ffbackend/bl/MultifactorService.java
+++ src/main/java/com/example/ffbackend/bl/MultifactorService.java
@@ -3,6 +3,7 @@ package com.example.ffbackend.bl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import com.example.ffbackend.vo.MultifactorDetailsVo;
 import com.example.ffbackend.vo.StockTipVo;
@@ -33,22 +34,54 @@ public class MultifactorService {
     public MultifactorDetailsVo getMultifactorDetails(Integer userId) {
         // TODO: 对接rpc
 
-        MultifactorDetailsVo res = new MultifactorDetailsVo(30020.5, 10000.0, null);
+        MultifactorDetailsVo res = new MultifactorDetailsVo(30020.5, 0.6, null);
         var portfolio = portfolioService.getPortfolio(userId);
         List<MultifactorDatasetVo> multifactorDatasets = new ArrayList<MultifactorDatasetVo>(portfolio.size());
+
+        var factors = new ArrayList<HashMap<String, Double>>() {
+            private static final long serialVersionUID = 3126410076252489098L;
+            {
+                add(new HashMap<String, Double>() {
+                    private static final long serialVersionUID = 3126410076252489098L;
+                    {
+                        put("牛逼", 15.2);
+
+                    }
+                });
+                add(new HashMap<String, Double>() {
+                    private static final long serialVersionUID = 3126410076252489098L;
+                    {
+                        put("牛逼", 10.2);
+                    }
+                });
+                add(new HashMap<String, Double>() {
+                    private static final long serialVersionUID = 3126410076252489098L;
+                    {
+                        put("牛逼", 6.2);
+                    }
+                });
+                add(new HashMap<String, Double>() {
+                    private static final long serialVersionUID = 3126410076252489098L;
+                    {
+                        put("潮牛丸", 23.5);
+                        put("美与男", 2.4);
+                    }
+                });
+                add(new HashMap<String, Double>() {
+                    private static final long serialVersionUID = 3126410076252489098L;
+                    {
+                        put("海绵宝", 15.3);
+                    }
+                });
+            }
+        };
+        Random rand = new Random();
+
         for (var stockInPortfolio : portfolio)
-            multifactorDatasets
-                    .add(res.new MultifactorDatasetVo(stockService.getStockNameByCode(stockInPortfolio.getCode()),
-                            stockInPortfolio.getCode(), new HashMap<String, Double>() {
-								private static final long serialVersionUID = 3126410076252489098L;
-								{
-                                    put("牛逼因子", 15.2);
-                                    put("潮汕牛肉丸因子", 23.5);
-                                    put("海绵宝宝因子", 15.3);
-                                    put("美人鱼战士与企鹅男孩", 2.4);
-                                }
-                            }, stockService.getStockPriceByCode(stockInPortfolio.getCode()), stockInPortfolio.getNum(),
-                            500));
+            multifactorDatasets.add(res.new MultifactorDatasetVo(
+                    stockService.getStockNameByCode(stockInPortfolio.getCode()), stockInPortfolio.getCode(),
+                    factors.get(rand.nextInt(factors.size())),
+                    stockService.getStockPriceByCode(stockInPortfolio.getCode()), stockInPortfolio.getNum(), 500));
 
         res.setDataSource(multifactorDatasets);
         return res;
--- src/main/java/com/example/ffbackend/bl/StockService.java
+++ src/main/java/com/example/ffbackend/bl/StockService.java
@@ -1,10 +1,14 @@
 package com.example.ffbackend.bl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.example.ffbackend.vo.StockVo;
 
 import org.springframework.stereotype.Service;
+
+import lombok.var;
 
 @Service
 public class StockService {
@@ -13,12 +17,20 @@ public class StockService {
     }
 
     public String getStockNameByCode(String code) {
-        return "花旗银行";
+        List<String> names = new ArrayList<String>() {
+            private static final long serialVersionUID = 5396651156597209178L;
+            {
+                add("花旗银行");
+                add("南大食堂");
+                add("蟹老板蟹肉煲");
+            }
+        };
+        var rand = new Random();
+        return names.get(rand.nextInt(names.size()));
     }
 
-    public Double getStockPriceByCode (String code) {
+    public Double getStockPriceByCode(String code) {
         Random rand = new Random();
         return (rand.nextInt(60) + 600) / 100.0;
     }
 }
-
