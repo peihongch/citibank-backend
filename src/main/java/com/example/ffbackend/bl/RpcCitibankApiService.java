b87d50391b18f7f3c1c77bca55991404debd1268
steve <chph13420146901@gmail.com>
2019-09-15 16:52:39 +0800

    feat: add citibank api grpc client stub

--- src/main/java/com/example/ffbackend/bl/RpcCitibankApiService.java
+++ src/main/java/com/example/ffbackend/bl/RpcCitibankApiService.java
@@ -1,4 +1,9 @@
 package com.example.ffbackend.bl;
 
 public interface RpcCitibankApiService {
+    float stockPoundage(float dealAmount);
+
+    float futurePoundage(float dealAmount);
+
+    float optionsPoundage(int numOfPiece);
 }
--- src/main/java/com/example/ffbackend/rpc/RpcCitibankApiServiceImpl.java
+++ src/main/java/com/example/ffbackend/rpc/RpcCitibankApiServiceImpl.java
@@ -1,8 +1,32 @@
 package com.example.ffbackend.rpc;
 
 import com.example.ffbackend.bl.RpcCitibankApiService;
+import com.example.ffbackend.rpc.proto.CitibankApiGrpc.*;
+import com.example.ffbackend.rpc.proto.CitibankApiProto.*;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RpcCitibankApiServiceImpl implements RpcCitibankApiService {
+    @Autowired
+    private CitibankApiBlockingStub citibankApiBlockingStub;
+
+    @Override
+    public float stockPoundage(float dealAmount) {
+        var input = StockPoundageInput.newBuilder().setDealAmount(dealAmount).build();
+        return citibankApiBlockingStub.stockPoundage(input).getValue();
+    }
+
+    @Override
+    public float futurePoundage(float dealAmount) {
+        var input = FuturePoundageInput.newBuilder().setDealAmount(dealAmount).build();
+        return citibankApiBlockingStub.futurePoundage(input).getValue();
+    }
+
+    @Override
+    public float optionsPoundage(int numOfPiece) {
+        var input = OptionsPoundageInput.newBuilder().setNumOfPiece(numOfPiece).build();
+        return citibankApiBlockingStub.optionsPoundage(input).getValue();
+    }
 }
