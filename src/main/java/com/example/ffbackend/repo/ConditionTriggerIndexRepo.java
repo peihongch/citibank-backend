c6a5751711495ac7b4a75654ddeecc77433593d2
HLin Xu <32236576+SimpleLonely@users.noreply.github.com>
2019-10-01 01:57:20 +0800

    modify

--- src/main/java/com/example/ffbackend/repo/ConditionTriggerIndexRepo.java
+++ src/main/java/com/example/ffbackend/repo/ConditionTriggerIndexRepo.java
@@ -5,8 +5,13 @@ import java.util.List;
 import com.example.ffbackend.entity.ConditionTriggerIndex;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ConditionTriggerIndexRepo extends JpaRepository<ConditionTriggerIndex, Integer> {
     List<ConditionTriggerIndex> findByUserId (Integer userId);
+
+    @Modifying
+    @Transactional
     void deleteByUserId (Integer userId);
 }
